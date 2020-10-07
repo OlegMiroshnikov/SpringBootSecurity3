99666d01280093b697f7f7d6eab45b5ba3b10160
Oleg Miroshnikov <oleg.psoft@gmail.com>
2020-10-07 22:03:58 +0300

    Initial commit - Basic Authentication

---
+++ src/test/java/lv/partnersoft/training/springbootsecurity3/Springbootsecurity3ApplicationTests.java
@@ -0,0 +1,13 @@
+package lv.partnersoft.training.springbootsecurity3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootsecurity3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
